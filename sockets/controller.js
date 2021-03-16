7ac85a52c745efc0d42ac53b1b5b8c45fe7f5fba
Nicolas Bertini Argañaras <nicolas_bertini@hotmail.com>
2021-03-16 12:37:06 -0300

    Sistema de gestion de turnos para un local. NO cuenta con autenticaciones

---
+++ sockets/controller.js
@@ -0,0 +1,48 @@
+const TicketControl = require("../models/ticket-control");
+
+const ticketControl = new TicketControl();
+
+const socketController = (socket) => {
+  //Cuando un cliente se conecta
+  socket.emit("ultimo-ticket", ticketControl.ultimo);
+  socket.emit("estado-actual", ticketControl.ultimos4);
+  socket.emit("tickets-pendientes", ticketControl.tickets.length);
+
+  socket.on("siguiente-ticket", (payload, callback) => {
+    const siguiente = ticketControl.siguiente();
+    callback(siguiente);
+    socket.broadcast.emit("tickets-pendientes", ticketControl.tickets.length);
+  });
+
+  socket.on("atender-ticket", ({ escritorio }, callback) => {
+    if (!escritorio) {
+      return callback({
+        ok: false,
+        msg: "El escritorio es OBLIGATORIO",
+      });
+    }
+    const ticket = ticketControl.atenderTicket(escritorio);
+    //Notificar Cambio en los ultimos4
+    socket.broadcast.emit("estado-actual", ticketControl.ultimos4);
+
+    //Para que la cola aparezca a todos, inclusive al escritorio actual
+    socket.emit("tickets-pendientes", ticketControl.tickets.length);
+    socket.broadcast.emit("tickets-pendientes", ticketControl.tickets.length);
+
+    if (!ticket) {
+      callback({
+        ok: false,
+        msg: "Ya no hay tickets pendientes!",
+      });
+    } else {
+      callback({
+        ok: true,
+        ticket,
+      });
+    }
+  });
+};
+
+module.exports = {
+  socketController,
+};
